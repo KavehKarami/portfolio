b1937c488bf4987b427f39a10691ad711ee028a3
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2023-06-09 09:50:52 +0330

    :sparkles: add head title

--- pages/index.page.js
+++ pages/index.page.js
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import { AnimatedCursor } from "@/components/AnimatedCursor";
 import Matrix from "@/components/Matrix";
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>Kaveh Karami CV</title>
+      </Head>
       <Matrix />
       <AnimatedCursor />
     </main>
